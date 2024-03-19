2cb2210ac42e3e0d2986ae068c1c908fd9997b3d
siesta <85063187+siestaaaa@users.noreply.github.com>
2024-03-20 02:21:08 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6468f87e-3b4c-4307-b4a7-a2d4e7e659d0/)
 
 
 echo "$curl_output" >> Update.tsx
